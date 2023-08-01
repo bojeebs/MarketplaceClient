643a7d7eb5b417900ee043c122b560f443804def
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-08-01 09:04:22 -0500

    Auth services file updated

---
+++ Client/marketplace/src/services/Auth.js
@@ -0,0 +1,31 @@
+import Client from "./api";
+
+export const LoginUser = async (data) => {
+  try { 
+    const res = await Client.post('api/login', data)
+    localStorage.setItem('token', res.data.token)
+    localStorage.setItem('id', res.data.user.id)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('api/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    const res = await Client.get('/api/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
